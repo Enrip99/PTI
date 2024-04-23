d52faa6580df30a35feb7a83778d3c128b132c53
enric.carpintero <enric.carpintero@estudiantat.upc.edu>
2024-04-23 16:08:02 +0200

    test for login

--- server_backend/mqttest.js
+++ server_backend/mqttest.js
@@ -1,9 +1,22 @@
 const mqtt = require("mqtt");
+const schedule = require('node-schedule');
 //const client = mqtt.connect("mqtt://localhost:8082");
 const client = mqtt.connect("mqtt://nattech.fib.upc.edu:40412");
 
 client.on("connect", () => {
-    client.publish("temperature", "Hola chavalada que tal estais");
-    client.end();
-    //process.exit();
+    const job = schedule.scheduleJob('*/30 * * * * *', function(fireDate){
+      client.publish("notification", "Prova de notificació a les" + fireDate );
+      });
+    //client.end();
+    client.subscribe("notification", (err) => {
+      if (err) {
+        console.error(err);
+        process.exit(3);
+      }
+    });
+});
+
+client.on("message", (topic, message) => {
+  // message is Buffer
+  console.log(topic, message.toString());
 });
